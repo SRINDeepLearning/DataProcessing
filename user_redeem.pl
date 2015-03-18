afc1ec2c5243e7720ef5259890d263ec4cc28d19
tanyani <tanya.nabila@gmail.com>
2015-03-18 13:54:34 +0700

    Update user_redeem.pl
    user_id|totalredeem|totalclaimed

--- user_redeem.pl
+++ user_redeem.pl
@@ -4,25 +4,51 @@ use strict;
 use warnings;
 
 
-open (doc, 'campaign_redeem-update.csv') or die "Can't open!";
-open(my $out, '>user_redeem.txt');
+open (main, 'campaign_claim-update.csv') or die "Can't open!";
+open(my $out, '>user_claim.txt');
 
-my %user_redeem;
+my @claimed;
 
-while(my $line = <doc>){
+while(my $line = <main>){
 	chomp $line;
 	my @entry = split(/\|/, $line);
 	
-	$user_redeem{$entry[8]}++;
+	push @claimed, $entry[3];
 }
 
+close main;
+open(redeem, 'campaign_redeem-update.csv') or die "Can't open!";
 
-foreach my $a (keys %user_redeem){
-	print $out $a."|".$user_redeem{$a}."\n";
+my %redeemed_user;
+my %redeem_code;
+my %claimed_user;
+while(my $line = <redeem>){
+	chomp $line;
+	my @entry = split(/\|/, $line);
+
+	$redeemed_user{$entry[8]}++;
+	$redeem_code{$entry[3]} = $entry[8];
+	$claimed_user{$entry[8]} = 0;
 }
 
-my @users = keys %user_redeem;
+foreach my $a (@claimed){
+	$claimed_user{$redeem_code{$a}}++;
+}
+
+print $out "user_id|totalredeem|totalclaimed\n";
+foreach my $a (keys %redeemed_user){
+	print $out $a."|".$redeemed_user{$a}."|".$claimed_user{$a}."\n";
+}
+my @users = keys %claimed_user;
 my $totalusers = @users;
-print $out "Total Users: ".$totalusers."\n";
+print $out "Total Unique Redeemed Users: ".$totalusers."\n";
 
-close doc;
+my @users_c;
+foreach my $a (keys %claimed_user){
+	if(%claimed_user{$a} != 0){
+		push @users_c, $a;
+	}
+}
+my $users_c = @users_c;
+print $out "Total Unique Claimed Users: ".$users_c."\n";
+
